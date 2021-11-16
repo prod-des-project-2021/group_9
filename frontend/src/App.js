fc66e36c2adf4462d46573f7e298f389d435ed20
Iikka <t8kuii00@students.oamk.fi>
2021-11-16 11:22:01 +0200

    Simple router and two placeholder pages implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,28 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import React from 'react'
+import Home from './components/pages/Home'
+import Recipes from './components/pages/Recipes'
 
 function App() {
+
+    const padding = {
+        padding: 5
+    }
     return (
-        <div>
-            <h1>Hello World</h1>
-            <p>This is the beginning of our ReseptiApp, let's goooooo 8)</p>
-        </div>
-    );
+        <Router>
+            {/* simple navbar for multiple pages for now... */}
+            <div>
+                <Link style={padding} to="/">home</Link>
+                <Link style={padding} to="/recipes">recipes</Link>
+            </div>
+
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/recipes" element={<Recipes />} />
+            </Routes>
+
+        </Router>
+    )
 }
 
-export default App;
+export default App
---
+++ frontend/src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <p>This is the beginning of our ReseptiApp, let's goooooo 8)</p>
+        </div>
+    );
+};
+
+export default Home;
