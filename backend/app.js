aafd51538b725d652ed6c7c074c0c09e31b1afd5
Iikka <t8kuii00@students.oamk.fi>
2021-11-19 10:22:07 +0200

    Cleaned up the index and moved  Express app on its on file...

---
+++ backend/app.js
@@ -0,0 +1,35 @@
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+
+const recipesRouter = require('./controllers/recipes')
+// const usersRouter = require('./controllers/users')
+
+const mongoose = require('mongoose')
+
+mongoose.connect(config.MONGODB_URI)
+    .then(() => {
+        logger.print('connected to MongoDB')
+    })
+    .catch((err) => {
+        logger.print('errror connecting to MongoDB:', err.message)
+    })
+
+app.use(express.static('build'))
+app.use(express.json())
+morgan.token('data', (req) => {
+    if (req.method === 'POST')
+        return JSON.stringify(req.body)
+    else
+        return
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+
+app.use('/api/recipes', recipesRouter)
+//app.use('/api/users', usersRouter)
+
+
+
+module.exports = app
