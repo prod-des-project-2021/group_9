0c73713992376106a58337a60af6a225f48f38b2
niinasocii@gmail.com <yKk73v9EKsZWLXn>
2021-11-22 15:35:47 +0200

    Image upload component created

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import ImageUpload from '../ImageUpload'
+
 
 const Home = () => {
     return (
         <div>
             <h1>Hello World</h1>
             <p>This is the beginning of our ReseptiApp, let's goooooo 8)</p>
+            <ImageUpload/>
         </div>
     );
 };
