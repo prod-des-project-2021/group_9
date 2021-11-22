7b7f93b5c825eaafc6ac4d0248f3c90d1aa2c4b9
Iikka <t8kuii00@students.oamk.fi>
2021-11-22 11:24:47 +0200

    crud for recipes

--- backend/controllers/recipes.js
+++ backend/controllers/recipes.js
@@ -1,4 +1,5 @@
 const recipesRouter = require('express').Router()
+const { response } = require('../app')
 const Recipe = require('../models/recipe')
 
 const createRecipe = require('../utils/foodgen')
@@ -9,17 +10,33 @@ recipesRouter.get('/', async (req, res) => {
     res.json(recipes)
 })
 
-// Post a recipe
+// Post
 recipesRouter.post('/', async (req, res) => {
     const newRecipe = new Recipe(req.body)
     const savedRecipe = await newRecipe.save()
     res.status(201).json(savedRecipe)
 })
 
+// Update
+recipesRouter.put('/:id', async (req, res) => {
+    const id = req.params.id
+    const updatedRecipe = req.body
+    const savedRecipe = await Recipe.findByIdAndUpdate(id, updatedRecipe, { new: true })
+    response.status(200).json(savedRecipe)
+})
+
+// Delete
+recipesRouter.put('/:id', async (req, res) => {
+    const id = req.params.id
+    const result = await Recipe.findByIdAndRemove(id)
+    response.status(200).json(result)
+})
+
+// Generate recipes
 recipesRouter.get('/create', async (req, res) => {
     for (let i = 0; i < 4; i++) {
-        // const newRecipe = new Recipe()
-        // await newRecipe.save()
+        const newRecipe = new Recipe()
+        await newRecipe.save()
         console.log(createRecipe())
     }
     const recipes = await Recipe.find({})
