2a4a2e4c8c8c05f4856786ead1eef68868cfed4a
Iikka <t8kuii00@students.oamk.fi>
2021-11-25 13:37:40 +0200

    restructuring... removed exppressvalidator

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,27 @@
+// utils
 const config = require('./utils/config')
 const logger = require('./utils/logger')
+
+// express
 const express = require('express')
 const app = express()
-const morgan = require('morgan')
-const cloudinary = require('cloudinary').v2
-const recipesRouter = require('./controllers/recipes')
-// const usersRouter = require('./controllers/users')
-const mongoose = require('mongoose')
 const expressValidator = require('express-validator')
 
+// middleware
+const cors = require('cors')
+const morgan = require('morgan')
+
+// routes
+const recipesRouter = require('./controllers/recipes')
+const usersRouter = require('./controllers/users')
+
+// databases
+const mongoose = require('mongoose')
+const cloudinary = require('cloudinary').v2
+
+
 cloudinary.config(config.cloudinaryConfig)
+
 
 mongoose.connect(config.MONGODB_URI)
     .then(() => {
@@ -18,36 +30,20 @@ mongoose.connect(config.MONGODB_URI)
     .catch((err) => {
         logger.print('errror connecting to MongoDB:', err.message)
     })
-
+app.use(cors({ origin: "*" }))
 app.use(express.static('build'))
 app.use(express.json())
-app.use(expressValidator({
-    customValidators: {
-        isImage: (value, filename) => {
-            const extension = (path.extname(filename)).toLowerCase()
-            switch (extension) {
-                case '.jpg':
-                    return '.jpg'
-                case '.jpeg':
-                    return '.jpeg'
-                case '.png':
-                    return '.png'
-                default:
-                    return false;
-            }
-        }
-    }
-}))
+
 morgan.token('data', (req) => {
     if (req.method === 'POST')
         return JSON.stringify(req.body)
     else
         return
 })
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+app.use(morgan(':method :remote-addr :url :status :response-time ms :data'))
 
 app.use('/api/recipes', recipesRouter)
-//app.use('/api/users', usersRouter)
+app.use('/api/users', usersRouter)
 
 
 
