72b2d3ffd20d49c620b810bca418d623ad0b7b8f
Juho Laukka <t9laju04@students.oamk.fi>
2021-12-02 12:11:27 +0200

    very basic login and register UI done

--- frontend/src/components/LogIn.js
+++ frontend/src/components/LogIn.js
@@ -1,60 +1,79 @@
 import React, { useState, useEffect } from 'react';
 
-const Home = () => {
-    const [register, setRegister] = useState(true);
+const LogIn = () => {
+    const [register, setRegister] = useState(false);
+
+    const switchFormHandler = (value) => () => {
+        setRegister(value);
+    }
 
     return (
         <div className="fixed t-0 w-full h-screen bg-gray-800 bg-opacity-60">
             <div className="relative h-full grid place-items-center">
-                <div className="relative bg-gray-300 rounded-2xl w-1/4 h-auto pb-24 shadow-2xl">
+                <div className="relative bg-gray-300 rounded-2xl w-72 md:w-96 h-auto pb-12 shadow-2xl">
                     <button className="absolute top-2 right-2 bg-gray-50 rounded-full w-6">.</button>
 
                     {!register
-                    ? <LoginForm />
-                    : <RegisterForm />
+                    ? <LoginForm switchFormHandler={switchFormHandler(true)} />
+                    : <RegisterForm switchFormHandler={switchFormHandler(false)} />
                     }
                     
                 </div>
             </div>
         </div>
     );
-};
+}
 
-const LoginForm = () => {
+const LoginForm = ({switchFormHandler}) => {
     return(
         <div>
             <div className="px-6 pt-4">
                 <h1>Login</h1>
             </div>
+
             <hr className="border-0 bg-yellow-400 h-px mb-4"/>
-            <div className="flex justify-center px-6">
+
+            <div className="flex-row justify-center space-y-2 px-6">
                 <form className="flex-row justify-center space-y-2">
                     <Input type="text" placeholder="Email or Username" />
                     <Input type="password" placeholder="Password" />
-                    <button className="bg-yellow-400 rounded-xl p-2 w-full text-center">Login</button>
+                    <button className="bg-yellow-400 hover:bg-yellow-200 rounded-xl p-2 w-full text-center shadow-md">Login</button>
                 </form>
+
+                <button
+                onClick={switchFormHandler}
+                className="bg-gray-50 hover:bg-yellow-200 rounded-xl p-2 w-full text-center shadow-md">
+                    Register
+                </button>
             </div>
         </div>
     );
-};
+}
 
-const RegisterForm = () => {
+const RegisterForm = ({switchFormHandler}) => {
     return(
         <div>
             <div className="px-6 pt-4">
                 <h1>Register</h1>
             </div>
-            <hr className="border-0 bg-yellow-400 h-px my-4"/>
-            <div className="flex justify-center px-6">
+
+            <hr className="border-0 bg-yellow-400 h-px mb-4"/>
+
+            <div className="flex-row justify-center space-y-2 px-6">
                 <form className="flex-row justify-center space-y-2">
                     <Input type="text" placeholder="Email" />
                     <Input type="text" placeholder="Username" />
                     <Input type="password" placeholder="Password" />
                     <Input type="password" placeholder="Password again" />
-                    <button className="bg-yellow-400 rounded-xl p-2 w-full text-center">Register</button>
+                    <button className="bg-yellow-400 hover:bg-yellow-200 rounded-xl p-2 w-full text-center">Register</button>
                 </form>
+
+                <button
+                onClick={switchFormHandler}
+                className="bg-gray-50 hover:bg-yellow-200 rounded-xl p-2 w-full text-center shadow-md">
+                    Login
+                </button>
             </div>
-            <hr className="border-0 bg-yellow-400 h-px my-4"/>
         </div>
     );
 }
@@ -69,4 +88,4 @@ const Input = ({type, placeholder}) => {
     );
 }
 
-export default Home;
+export default LogIn;
