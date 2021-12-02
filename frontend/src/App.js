f3b79b3e56bba3a38cc1cad5638307ca7950cdc1
Iikka <t8kuii00@students.oamk.fi>
2021-12-02 15:44:18 +0200

    wrapped app inside provider for redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,24 +7,42 @@ import Navbar from './components/Navbar';
 import MyRecipes from './components/pages/MyRecipes'
 import Footer from './components/Footer';
 
+import { Provider, useSelector, useDispatch } from 'react-redux'
+import store from './redux/store'
+import { logout } from './redux/actions/auth'
+
 function App() {
 
-    const padding = {
-        padding: 5
+    const { isLoggedIn, user } = useSelector(state => state.auth)
+    const dispatch = useDispatch()
+
+    const handleLogout = () => {
+        dispatch(logout())
     }
 
     return (
-        <Router>         
-            <LogIn />
-
-            <Navbar />
-                    <Routes>
-                        <Route exact path="/" element={<Home />} />
-                        <Route exact path="/recipes" element={<Recipes />} />
-                        <Route exact path="/myrecipes" element={<MyRecipes />} />
-                    </Routes>
+        <Router>
+            {!isLoggedIn && <LogIn />}
+            {console.log(user)}
+            <Navbar handleLogout={handleLogout} isLoggedIn={isLoggedIn} />
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/recipes" element={<Recipes />} />
+                <Route exact path="/myrecipes" element={<MyRecipes />} />
+            </Routes>
+            <Footer />
         </Router>
     )
 }
 
-export default App
+// wrapper component to wrap our App inside Provider component
+// 'Provider' is used by redux states
+const AppWrapper = () => {
+    return (
+        <Provider store={store}>
+            <App />
+        </Provider>
+    )
+}
+
+export default AppWrapper
