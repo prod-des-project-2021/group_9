924f9b6785723acc10b05991b2332384f0490cd3
Iikka <t8kuii00@students.oamk.fi>
2021-12-08 12:40:24 +0200

    'user' field is now type mongoose.ObjectId

--- backend/models/recipe.js
+++ backend/models/recipe.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose')
 
 const recipeSchema = new mongoose.Schema({
     name: String,
-    user: String,
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
     ratings: [],
     ingredients: [],
     image: String,
