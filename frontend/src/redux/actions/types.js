ddd61c5001aae3110c83f08dc3557848abcdcd8b
niinasocii@gmail.com <yKk73v9EKsZWLXn>
2021-12-10 13:35:16 +0200

    adding set shopping list to types

--- frontend/src/redux/actions/types.js
+++ frontend/src/redux/actions/types.js
@@ -6,3 +6,6 @@ export const LOGOUT = "LOGOUT";
 
 export const SET_MESSAGE = "SET_MESSAGE";
 export const CLEAR_MESSAGE = "CLEAR_MESSAGE";
+
+export const SET_SHOPPINGLIST = "SET_SHOPPINGLIST";
+export const CLEAR_SHOPPINGLIST = "CLEAR_SHOPPINGLIST";
