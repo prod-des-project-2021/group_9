2611b28ea8dc174bc7d5377ddca2470ba918328d
niinasocii@gmail.com <yKk73v9EKsZWLXn>
2021-12-17 12:39:24 +0200

    Clean myrecipes & add delete button to recipe.js

--- frontend/src/components/pages/Recipe.js
+++ frontend/src/components/pages/Recipe.js
@@ -22,108 +22,122 @@ const Recipe = () => {
             });
     }, [location]);
 
-    return (
-        <div>
-            <RecipeInfo recipe={recipe} />
-        </div>
-    );
-};
 
-const RecipeInfo = ({ recipe }) => {
-    if (recipe === null) { // If the given recipe is null, then show a placeholder box.
+    const deleteRecipeHandler = (recipe) => () => {
+        recipeService
+            .deleteRecipe(recipe.id)
+            .then(initialRecipes => {
+
+            });
         return (
-            <div className="p-8 lg:mx-24">
-                NOTHING
+            <div>
+                <RecipeInfo recipe={recipe} />
             </div>
         );
-    }
-    else { // if the given recipe is NOT null, then show its info.
-        return (
-            <div className="relative p-12 pb-24 field lg:mx-24">
-                <h1>{recipe.name}</h1>
+    };
 
-                {/* Show ingredients and instructions side by side (for now). */}
-                <div className="md:flex md:mx-6 space-y-6 md:space-x-6 md:space-y-0">
-                    <IngredientList recipe={recipe} />
-                    <Instructions recipe={recipe} />
+    const RecipeInfo = ({ recipe }) => {
+        if (recipe === null) { // If the given recipe is null, then show a placeholder box.
+            return (
+                <div className="p-8 lg:mx-24">
+                    NOTHING
                 </div>
-            </div>
-        );
-    }
-}
+            );
+        }
+        else { // if the given recipe is NOT null, then show its info.
+            return (
+                <div className="relative p-12 pb-24 field lg:mx-24">
+                    <h1>{recipe.name}</h1>
+                    <div className="flex absolute md:top-4 right-4 space-x-2">
+                        <button
+                            onClick={deleteRecipeHandler(recipe)} // Call deleteHandler when clicked.
+                            className="bg-gray-500 hover:bg-red-400 p-4 shadow-md w-auto">
+                            DELETE
+                        </button>
+                    </div>
 
-// Ingredients of the given recipe are listed.
-const IngredientList = ({ recipe }) => {
-
-    const dispatch = useDispatch()
-
-    const { isLoggedIn } = useSelector(state => state.auth)
-
-    const clickHandler = (ingredient) => () => {
-        if (isLoggedIn)
-            dispatch(setShoppingList(ingredient))
-
-        else {
-
-
+                    {/* Show ingredients and instructions side by side (for now). */}
+                    <div className="md:flex md:mx-6 space-y-6 md:space-x-6 md:space-y-0">
+                        <IngredientList recipe={recipe} />
+                        <Instructions recipe={recipe} />
+                    </div>
+                </div>
+            );
         }
     }
 
-    return (
-        <div className="md:w-1/2 shadow-t-md">
-            <ModeButton text="Ingredients" />
-            <table className="table-auto w-full">
-                <tbody className="divide-y">
-                    {/* Map function is used to display a list of ingredients. */}
-                    {recipe.ingredients.map(ingredient => <Ingredient key={ingredient.id} ingredient={ingredient} clickHandler={clickHandler} />)}
-                </tbody>
-            </table>
-        </div>
-    );
+    // Ingredients of the given recipe are listed.
+    const IngredientList = ({ recipe }) => {
+
+        const dispatch = useDispatch()
+
+        const { isLoggedIn } = useSelector(state => state.auth)
+
+        const clickHandler = (ingredient) => () => {
+            if (isLoggedIn)
+                dispatch(setShoppingList(ingredient))
+
+            else {
+
+
+            }
+        }
+
+        return (
+            <div className="md:w-1/2 shadow-t-md">
+                <ModeButton text="Ingredients" />
+                <table className="table-auto w-full">
+                    <tbody className="divide-y">
+                        {/* Map function is used to display a list of ingredients. */}
+                        {recipe.ingredients.map(ingredient => <Ingredient key={ingredient.id} ingredient={ingredient} clickHandler={clickHandler} />)}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+
+    // A single ingredient. Consists of amount, unit and the name of the ingredient.
+    const Ingredient = ({ ingredient, clickHandler }) => {
+
+
+        return (
+            <tr>
+                <td className="w-24 p-2 text-right">{ingredient.amount} {ingredient.unit}</td>
+                <td className="p-2">{ingredient.name}</td>
+                <td>
+                    <button onClick={clickHandler(ingredient)}> + </button>
+                </td>
+
+            </tr>
+        );
+    }
+
+    // Instructions of the given recipe are listed.
+    // WIP (recipes don't have instructions yet).
+    const Instructions = ({ recipe }) => {
+        if (!recipe.steps)
+            return null;
+
+        return (
+            <div className="table-auto md:w-1/2 shadow-t-md">
+                <ModeButton text="Instructions" />
+                <ul className="list-disc m-4 space-y-2">
+                    {recipe.steps.map(step => <li key={step.id}>{step.text}</li>)}
+                </ul>
+            </div>
+        );
+    }
+
+    const ModeButton = ({ text }) => {
+
+        return (
+            <button
+                className="bg-yellow-200 rounded-t-xl px-4 text-black">
+                {text}
+            </button>
+        );
+    }
+
 }
-
-// A single ingredient. Consists of amount, unit and the name of the ingredient.
-const Ingredient = ({ ingredient, clickHandler }) => {
-
-
-    return (
-        <tr>
-            <td className="w-24 p-2 text-right">{ingredient.amount} {ingredient.unit}</td>
-            <td className="p-2">{ingredient.name}</td>
-            <td>
-                <button onClick={clickHandler(ingredient)}> + </button>
-            </td>
-
-        </tr>
-    );
-}
-
-// Instructions of the given recipe are listed.
-// WIP (recipes don't have instructions yet).
-const Instructions = ({ recipe }) => {
-    if (!recipe.steps)
-        return null;
-
-    return (
-        <div className="table-auto md:w-1/2 shadow-t-md">
-            <ModeButton text="Instructions" />
-            <ul className="list-disc m-4 space-y-2">
-                {recipe.steps.map(step => <li key={step.id}>{step.text}</li>)}
-            </ul>
-        </div>
-    );
-}
-
-const ModeButton = ({ text }) => {
-
-    return (
-        <button
-            className="bg-yellow-200 rounded-t-xl px-4 text-black">
-            {text}
-        </button>
-    );
-}
-
-
 
 export default Recipe;
