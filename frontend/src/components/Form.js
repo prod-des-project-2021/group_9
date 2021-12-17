8ef20a670d37f27a500a15f8e99076c950fe07af
Kristian Ängeslevä <kristian.angesleva@gmail.com>
2021-12-17 16:20:54 +0200

    Recipe pop up final version (need to tweak create new recipe position in frontend after this)

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -13,25 +13,38 @@ import ImageUpload from './ImageUpload'
 
 const useStyles = makeStyles((theme) => ({
   root: {
+
     '& .MuiTextField-root': {
-      margin: theme.spacing(1),
+      background: '#D3D3D3',
+      color: '',
+      margin: theme.spacing(0.5),
     },
   },
   button: {
+    color: 'black',
+    background: '#A9A9A9',
     margin: theme.spacing(1),
+  },
+  row: {
+    width: '100%'
   }
 }))
-
-
 
 function Form() {
   const classes = useStyles()
 
-  
   const [recipe, setRecipe] = useState({
-    name: '' ,
+    name: '',
     ingredients: [{ id: uuidv4(), name: '', amount: '', unit: '' }],
     steps: [{ id: uuidv4(), text: '' }]
+
+  });
+
+  const [error, setError] = useState({
+    name: true,
+    ingredients: [{ index: 0, valid: true }],
+    steps: [{ index: 0, valid: true }]
+
   });
 
   const [image, setImage] = useState(null)
@@ -46,12 +59,39 @@ function Form() {
     /* formData.append('recipe', recipe) */
 
     console.log(recipe);
-    recipeService.create(formData).then()
 
+    if (validate()) {
+      recipeService.create(formData).then()
+    }
   };
 
+  function validate() {
+    let isValid = true
+    let er = { name: true, ingredients: [], steps: [] }
+    if (recipe.name === "") {
+      isValid = false
+      er.name = false
+    }
+    for (let i = 0; i < recipe.steps.length; i++) {
+      er.steps.push(true)
+      if (recipe.steps[i].text === "") {
+        er.steps[i] = false
+        isValid = false
+      }
+    }
+    for (let i = 0; i < recipe.ingredients.length; i++) {
+      er.ingredients.push(true)
+      if (recipe.ingredients[i].name === "") {
+        er.ingredients[i] = false
+        isValid = false
+      }
+    }
+    setError(er)
+    return isValid
+  }
+
   const handleChangeInput = (id, event) => {
-    const newRecipe = {...recipe}
+    const newRecipe = { ...recipe }
     newRecipe.ingredients = recipe.ingredients.map(i => {
       if (id === i.id) {
         i[event.target.name] = event.target.value
@@ -62,7 +102,7 @@ function Form() {
   }
 
   const handleChangeInput2 = (id, event) => {
-    const newRecipe = {...recipe}
+    const newRecipe = { ...recipe }
     newRecipe.steps = recipe.steps.map(i => {
       if (id === i.id) {
         i.text = event.target.value
@@ -73,101 +113,132 @@ function Form() {
   }
 
   const handleChangeInput3 = (event) => {
-    const newRecipe = {...recipe}
-    newRecipe.name = event.target.value 
+    const newRecipe = { ...recipe }
+    newRecipe.name = event.target.value
     setRecipe(newRecipe)
   }
 
-
   const handleAddFields = () => {
-    const newRecipe = {...recipe}
+    const newRecipe = { ...recipe }
     newRecipe.ingredients.push({ id: uuidv4(), name: '', amount: '', unit: '' })
     setRecipe(newRecipe)
+
+    const newError = { ...error }
+    newError.ingredients.push(true)
+    setError(newError)
   }
 
   const handleAddFields2 = () => {
-    const newRecipe = {...recipe}
+    const newRecipe = { ...recipe }
     newRecipe.steps.push({ id: uuidv4(), text: '' })
     setRecipe(newRecipe)
+
+    const newError = { ...error }
+    newError.steps.push(true)
+    setError(newError)
   }
 
-
-
   const handleRemoveFields = id => {
-    const newRecipe = {...recipe}
+    const newRecipe = { ...recipe }
+
+    const newError = { ...error }
+    newError.ingredients.splice(newRecipe.ingredients.findIndex(ingredient => ingredient.id === id), 1);
+    setError(newError)
+
     newRecipe.ingredients.splice(newRecipe.ingredients.findIndex(ingredient => ingredient.id === id), 1);
     setRecipe(newRecipe);
   }
 
   const handleRemoveFields2 = id => {
-    const newRecipe = {...recipe}
+    const newRecipe = { ...recipe }
+
+    const newError = { ...error }
+    newError.steps.splice(newRecipe.steps.findIndex(step => step.id === id), 1);
+    setError(newError)
+
     newRecipe.steps.splice(newRecipe.steps.findIndex(step => step.id === id), 1);
     setRecipe(newRecipe);
   }
-
-
 
   const imageCallback = (image) => {
     setImage(image)
   }
 
-
+  const styles = {
+    root: {
+      background: "black"
+    },
+    input: {
+      color: "white"
+    }
+  };
 
   return (
-    <Container>
-      
+    <Container
+    style={{
+      borderRadius: 20,
+      color: '#000000',
+      backgroundColor: "#F5D142",
+  }}>
       <form className={classes.root} onSubmit={handleSubmit}>
-          <div>
+        <div>
           <h1>Add name</h1>
-            <TextField          
-              name="name"
-              label="Recipe name"
-              variant="filled"
-              value={recipe.name}
-              onChange={handleChangeInput3}
-            />
-            <div>{recipe.name}</div>
-          </div>
+          <TextField
+            className={classes.root}
+            InputProps={{
+              className: classes.input
+            }}
+            name="name"
+            label="Recipe name"
+            variant="filled"
+            value={recipe.name}
+            onChange={handleChangeInput3}
+          />
+        </div>
+        {error.name ? null : <div className='text-red-500'>*Recipe name is required</div>}
         <h1>Add ingredients</h1>
-        {recipe.ingredients.map(ingredient => (
+        {recipe.ingredients.map((ingredient, i) => (
           <div key={ingredient.id}>
-            <TextField
-              name="name"
-              label="Ingredient Name"
-              variant="filled"
-              value={ingredient.name}
-              onChange={event => handleChangeInput(ingredient.id, event)}
-            />
-            <TextField
-              name="amount"
-              label="Amount"
-              variant="filled"
-              value={ingredient.amount}
-              onChange={event => handleChangeInput(ingredient.id, event)}
-            />
-            <TextField
-              name="unit"
-              label="Unit"
-              variant="filled"
-              value={ingredient.unit}
-              onChange={event => handleChangeInput(ingredient.id, event)}
-            />
-            <IconButton disabled={recipe.ingredients.length === 1} onClick={() => handleRemoveFields(ingredient.id)}>
-              <RemoveIcon />
-            </IconButton>
-            <IconButton
-              onClick={handleAddFields}
-            >
-              <AddIcon />
-            </IconButton>
+            <div className={classes.row}>
+              <TextField
+                backgroundColor="#E7CE19"
+                name="name"
+                label="Ingredient Name"
+                variant="filled"
+                value={ingredient.name}
+                onChange={event => handleChangeInput(ingredient.id, event)}
+              />
+              <TextField
+                name="amount"
+                label="Amount"
+                variant="filled"
+                value={ingredient.amount}
+                onChange={event => handleChangeInput(ingredient.id, event)}
+              />
+              <TextField
+                name="unit"
+                label="Unit"
+                variant="filled"
+                value={ingredient.unit}
+                onChange={event => handleChangeInput(ingredient.id, event)}
+              />
+              <IconButton disabled={recipe.ingredients.length === 1} onClick={() => handleRemoveFields(ingredient.id)}>
+                <RemoveIcon />
+              </IconButton>
+              <IconButton
+                onClick={handleAddFields}
+              >
+                <AddIcon />
+              </IconButton>
+            </div>
+            {error.ingredients[i] ? null : <div className='text-red-500'>*Ingredient name is required</div>}
           </div>
         ))}
-
         <h1>Instructions</h1>
-        {recipe.steps.map(step => (
+        {recipe.steps.map((step, i) => (
           <div key={step.id}>
             <TextField
-              fullWidth = "90%"
+              fullWidth="90%"
               name="step"
               label="Step"
               variant="filled"
@@ -182,6 +253,7 @@ function Form() {
             >
               <AddIcon />
             </IconButton>
+            {error.steps[i] ? null : <div className='text-red-500'>*Step is required to be filled </div>}
           </div>
         ))}
         <h1>Upload picture of the food</h1>
@@ -191,7 +263,6 @@ function Form() {
           variant="contained"
           color="primary"
           type="submit"
-          endIcon={<Icon>send</Icon>}
           onClick={handleSubmit}
         >Save recipe</Button>
       </form>
